fd067c03a3b98d713299a1e91d8362cbaf92175a
ycohuiLastRoad <72780469+ycohuiLastRoad@users.noreply.github.com>
2020-10-18 18:28:42 +0800

    Update start.js
    restoration of part 2 test

--- hub/start.js
+++ hub/start.js
@@ -58,9 +58,3 @@ $(function(){
 function onBuilding() {
   alert("It's Still on Building, Sorry!");
   }
-function openChangeLog() {
-  document.getElementById("changeLog").style.display = "block";
-}
-function closeChangeLog {
-  document.getElementById("changeLog").style.display = "none";
-}
