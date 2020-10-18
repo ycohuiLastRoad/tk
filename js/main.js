c8ec16b74c1b50069d798948a61af7a4e65a318a
ycohuiLastRoad <72780469+ycohuiLastRoad@users.noreply.github.com>
2020-10-18 20:15:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// accordion (collapsable tab)
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight*100 + "px";
+    } 
+  });
+}
