bca3c3558574920f11dd4afe4ae29ff122bf3cb1
ycohuiLastRoad <72780469+ycohuiLastRoad@users.noreply.github.com>
2020-10-18 19:53:25 +0800

    Update main.js

--- hub/main.js
+++ hub/main.js
@@ -1,9 +1,26 @@
 function onBuilding() {
   alert("It's Still on Building, Sorry!");
-  };
+}
 function cLopen() {
   document.getElementById("changeLog").style.display = "block";
-};
+}
 function cLclose(){
   document.getElementById("changeLog").style.display = "none";
-};
+}
+
+
+// accordion (collapsable tab)
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight*100 + "px";
+    } 
+  });
+}
