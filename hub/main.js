2a3b437f3f6d7e12c78651ffd3d5497fc14d78c4
ycohuiLastRoad <72780469+ycohuiLastRoad@users.noreply.github.com>
2020-10-18 18:34:20 +0800

    Update main.js

--- hub/main.js
+++ hub/main.js
@@ -1,9 +1,9 @@
 function onBuilding() {
   alert("It's Still on Building, Sorry!");
   };
-function cLopen() {
-  document.getElementById("changeLog").style.display = "block";
-};
-function cLclose{
-  document.getElementById("changeLog").style.display = "none";
-};
+//function cLopen() {
+  //document.getElementById("changeLog").style.display = "block";
+//};
+//function cLclose{
+  //document.getElementById("changeLog").style.display = "none";
+//};
