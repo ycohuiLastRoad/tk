ce9300dcd0c8b1fd151d5a0890d67f7c63110870
ycohuiLastRoad <72780469+ycohuiLastRoad@users.noreply.github.com>
2020-10-18 17:28:09 +0800

    Update main.js

--- hub/main.js
+++ hub/main.js
@@ -1,3 +1,9 @@
 function onBuilding() {
   alert("It's Still on Building, Sorry!");
   }
+function openChangeLog() {
+  document.getElementById("changeLog").style.display = "block";
+}
+function closeChangeLog {
+  document.getElementById("changeLog").style.display = "none";
+}
