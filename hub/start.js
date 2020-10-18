2f08483dfae76b8f47aba4defb13fd359efb84d0
ycohuiLastRoad <72780469+ycohuiLastRoad@users.noreply.github.com>
2020-10-18 18:25:56 +0800

    Update start.js
    More copy from /hub/main.js

--- hub/start.js
+++ hub/start.js
@@ -54,6 +54,13 @@ $(function(){
     });
 })
 
+// Custiomized Functions
 function onBuilding() {
   alert("It's Still on Building, Sorry!");
   }
+function openChangeLog() {
+  document.getElementById("changeLog").style.display = "block";
+}
+function closeChangeLog {
+  document.getElementById("changeLog").style.display = "none";
+}
